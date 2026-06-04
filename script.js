effd878d024533565f8c6a93630f0a5575fd4211
Vishnu Ram <vishnuram4181@gmail.com>
2026-06-04 13:24:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,390 @@
+const songs = [
+    {
+        title: "Kaavaa Ulla",
+        file: "Music/Kaavaa Ulla.flac"
+    },
+    {
+        title: "Kasethan Kadavulada",
+        file: "Music/Kasethan Kadavulada.flac"
+    },
+    {
+        title: "Come Back Indian",
+        file: "Music/Come Back Indian.flac"
+    },
+    {
+        title: "Aadungada_Yennai_Suththi",
+        file: "Music/Aadungada_Yennai_Suththi.flac"
+    },
+    {
+        title: "Aale_Saachuputta",
+        file: "Music/Aale_Saachuputta.mp3"
+    },
+    {
+        title: "Anbe_Anbe",
+        file: "Music/Anbe_Anbe.mp3"
+    },
+    {
+        title: "Aye_Aye_Aye",
+        file: "Music/Aye_Aye_Aye.flac"
+    },
+    {
+        title: "Ayyayo",
+        file: "Music/Ayyayo.flac"
+    },
+    {
+        title: "Beep_Song",
+        file: "Music/Beep_Song.mp3"
+    },
+    {
+        title: "Beer_Song",
+        file: "Music/Beer_Song.flac"
+    },
+    {
+        title: "Bittu_Padam",
+        file: "Music/Bittu_Padam.mp3"
+    },
+    {
+        title: "Chilla_Chilla",
+        file: "Music/Chilla_Chilla.flac"
+    },
+    {
+        title: "Come_on_Girls",
+        file: "Music/Come_on_Girls.flac"
+    },
+    {
+        title: "Damaalu_Dumeelu",
+        file: "Music/Damaalu_Dumeelu.mp3"
+    },
+    {
+        title: "Dandanakka",
+        file: "Music/Dandanakka.mp3"
+    },
+    {
+        title: "Dhikku_Dhikku_Sir",
+        file: "Music/Dhikku_Dhikku_Sir.mp3"
+    },
+    {
+        title: "Evanda_Enakku_Custody",
+        file: "Music/Evanda_Enakku_Custody.flac"
+    },
+    {
+        title: "Hara_Hara_Mahadevaki",
+        file: "Music/Hara_Hara_Mahadevaki.mp3"
+    },
+    {
+        title: "Jalabulajangu",
+        file: "Music/Jalabulajangu.mp3"
+    },
+    {
+        title: "Kadhal_Sadugudu",
+        file: "Music/Kadhal_Sadugudu.flac"
+    },
+    {
+        title: "Kadhalan",
+        file: "Music/Kadhalan.mp3"
+    },
+    {
+        title: "Kanave_Kanave",
+        file: "Music/Kanave_Kanave.mp3"
+    },
+    {
+        title: "Kannai_Nambathey",
+        file: "Music/Kannai_Nambathey.mp3"
+    },
+    {
+        title: "Kannitheevu_Ponna",
+        file: "Music/Kannitheevu_Ponna.flac"
+    },
+    {
+        title: "Karuppu_Vellai",
+        file: "Music/Karuppu_Vellai.flac"
+    },
+    {
+        title: "Kona_Kondakari",
+        file: "Music/Kona_Kondakari.mp3"
+    },
+    {
+        title: "LED_Kannala",
+        file: "Music/LED_Kannala.mp3"
+    },
+    {
+        title: "Maane_Maane",
+        file: "Music/Maane_Maane.mp3"
+    },
+    {
+        title: "Machi_Engalukku",
+        file: "Music/Machi_Engalukku.mp3"
+    },
+    {
+        title: "Madura_Palapalakkuthu",
+        file: "Music/Madura_Palapalakkuthu.flac"
+    },
+    {
+        title: "Mascara",
+        file: "Music/Mascara.flac"
+    },
+    {
+        title: "Munnal_Kadhali",
+        file: "Music/Munnal_Kadhali.mp3"
+    },
+    {
+        title: "Naan_Konjam_Karuppu_Thaan",
+        file: "Music/Naan_Konjam_Karuppu_Thaan.mp3"
+    },
+    {
+        title: "Naan_Romba",
+        file: "Music/Naan_Romba.mp3"
+    },
+    {
+        title: "Naan_Sonnadhum",
+        file: "Music/Naan_Sonnadhum.flac"
+    },
+    {
+        title: "Nee_Yeppo_Pulla",
+        file: "Music/Nee_Yeppo_Pulla.mp3"
+    },
+    {
+        title: "Neruppu_Da",
+        file: "Music/Neruppu_Da.mp3"
+    },
+    {
+        title: "Onnum_Puriyala",
+        file: "Music/Onnum_Puriyala.mp3"
+    },
+    {
+        title: "Oxygen",
+        file: "Music/Oxygen.flac"
+    },
+    {
+        title: "Pazhangalla",
+        file: "Music/Pazhangalla.mp3"
+    },
+    {
+        title: "Penne_Naa_Enna_Solla",
+        file: "Music/Penne_Naa_Enna_Solla.mp3"
+    },
+    {
+        title: "Porkkalam",
+        file: "Music/Porkkalam.flac"
+    },
+    {
+        title: "Rangu_Rakkara",
+        file: "Music/Rangu_Rakkara.mp3"
+    },
+    {
+        title: "Sangili_Bungili",
+        file: "Music/Sangili_Bungili.flac"
+    },
+    {
+        title: "Suttum_Vizhi",
+        file: "Music/Suttum_Vizhi.flac"
+    },
+    {
+        title: "Takkaru_Takkaru",
+        file: "Music/Takkaru_Takkaru.mp3"
+    },
+    {
+        title: "Taxi_Taxi",
+        file: "Music/Taxi_Taxi.flac"
+    },
+    {
+        title: "Thakida_Thakida",
+        file: "Music/Thakida_Thakida.mp3"
+    },
+    {
+        title: "Ucchathula",
+        file: "Music/Ucchathula.mp3"
+    },
+    {
+        title: "Ullaallaa",
+        file: "Music/Ullaallaa.flac"
+    },
+    {
+        title: "Ussumu_Laresey",
+        file: "Music/Ussumu_Laresey.flac"
+    },
+    {
+        title: "Vaadi_Ne_Va",
+        file: "Music/Vaadi_Ne_Va.flac"
+    },
+    {
+        title: "Vaathi_Raid",
+        file: "Music/Vaathi_Raid.flac"
+    },
+    {
+        title: "Vasantha_Mullai",
+        file: "Music/Vasantha_Mullai.flac"
+    },
+    {
+        title: "Yemma_Yea_Alagamma",
+        file: "Music/Yemma_Yea_Alagamma.mp3"
+    }
+];
+
+let currentSong = 0;
+
+const audio = document.getElementById("audio");
+const playBtn = document.getElementById("playBtn");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+const progress = document.getElementById("progress");
+const songTitle = document.getElementById("songTitle");
+const volume = document.getElementById("volume");
+const playlist = document.getElementById("playlist");
+const currentTimeEl = document.getElementById("currentTime");
+const durationEl = document.getElementById("duration");
+const cover = document.querySelector(".cover");
+
+function loadSong(index) {
+
+    audio.src = songs[index].file;
+    songTitle.textContent = songs[index].title;
+
+    document.querySelectorAll("#playlist li").forEach(li => {
+        li.classList.remove("active-song");
+    });
+
+    const activeItem = document.querySelector(
+        `#playlist li[data-index="${index}"]`
+    );
+
+    if (activeItem) {
+        activeItem.classList.add("active-song");
+    }
+}
+
+songs.forEach((song, index) => {
+
+    const li = document.createElement("li");
+
+    li.textContent = song.title;
+    li.dataset.index = index;
+
+    li.addEventListener("click", () => {
+
+        currentSong = index;
+
+        loadSong(currentSong);
+
+        audio.play();
+
+        playBtn.textContent = "⏸";
+
+        cover.classList.add("playing");
+
+    });
+
+    playlist.appendChild(li);
+
+});
+
+loadSong(currentSong);
+
+playBtn.addEventListener("click", () => {
+
+    if (audio.paused) {
+
+        audio.play();
+
+        playBtn.textContent = "⏸";
+
+        cover.classList.add("playing");
+
+    } else {
+
+        audio.pause();
+
+        playBtn.textContent = "▶";
+
+        cover.classList.remove("playing");
+    }
+
+});
+
+nextBtn.addEventListener("click", () => {
+
+    currentSong++;
+
+    if (currentSong >= songs.length) {
+        currentSong = 0;
+    }
+
+    loadSong(currentSong);
+
+    audio.play();
+
+    playBtn.textContent = "⏸";
+
+    cover.classList.add("playing");
+
+});
+
+prevBtn.addEventListener("click", () => {
+
+    currentSong--;
+
+    if (currentSong < 0) {
+        currentSong = songs.length - 1;
+    }
+
+    loadSong(currentSong);
+
+    audio.play();
+
+    playBtn.textContent = "⏸";
+
+    cover.classList.add("playing");
+
+});
+
+audio.addEventListener("timeupdate", () => {
+
+    progress.value =
+        (audio.currentTime / audio.duration) * 100 || 0;
+
+    currentTimeEl.textContent =
+        formatTime(audio.currentTime);
+
+    durationEl.textContent =
+        formatTime(audio.duration || 0);
+
+});
+
+progress.addEventListener("input", () => {
+
+    audio.currentTime =
+        (progress.value / 100) * audio.duration;
+
+});
+
+volume.addEventListener("input", () => {
+
+    audio.volume = volume.value / 100;
+
+});
+
+audio.addEventListener("ended", () => {
+
+    currentSong++;
+
+    if (currentSong >= songs.length) {
+        currentSong = 0;
+    }
+
+    loadSong(currentSong);
+
+    audio.play();
+
+});
+
+function formatTime(seconds) {
+
+    let minutes = Math.floor(seconds / 60);
+    let secs = Math.floor(seconds % 60);
+
+    if (secs < 10) {
+        secs = "0" + secs;
+    }
+
+    return `${minutes}:${secs}`;
+}
